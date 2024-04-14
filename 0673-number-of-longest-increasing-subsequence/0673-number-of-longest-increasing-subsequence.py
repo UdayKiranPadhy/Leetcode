6a14ac4afb084cae06ed6114d24585f3a55e9a17
Uday Kiran <kiranu941@gmail.com>
2024-04-14 20:11:39 +0530

    Time: 554 ms (50.34%), Space: 16.9 MB (43.01%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.py
@@ -1,22 +1,19 @@
 class Solution:
-    def findNumberOfLIS(self, nums):
-        if not nums: return 0
-    
-        decks, ends_decks, paths = [], [], []
-        for num in nums:
-            deck_idx = bisect.bisect_left(ends_decks, num)
-            n_paths = 1
-            if deck_idx > 0:
-                l = bisect.bisect(decks[deck_idx-1], -num)
-                n_paths = paths[deck_idx-1][-1] - paths[deck_idx-1][l]
-                
-            if deck_idx == len(decks):
-                decks.append([-num])
-                ends_decks.append(num)
-                paths.append([0,n_paths])
-            else:
-                decks[deck_idx].append(-num)
-                ends_decks[deck_idx] = num
-                paths[deck_idx].append(n_paths + paths[deck_idx][-1])
-              
-        return paths[-1][-1]
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        N = len(nums)
+        dp = [1] * N
+        count = [1] * N
+
+        for i in range(N):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    if dp[i] < dp[j]+1:
+                        dp[i] = dp[j] + 1
+                        count[i] = count[j]
+                    elif dp[i] == dp[j] +1:
+                        count[i] += count[j]
+        maxi = max(dp)
+        ans = 0
+        for i in range(N):
+            if dp[i] == maxi: ans += count[i]
+        return ans
