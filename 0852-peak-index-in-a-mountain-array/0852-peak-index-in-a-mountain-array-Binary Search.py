633aaf0394f5c3fe7487f484ecbb0fa5850997d6
Uday Kiran <kiranu941@gmail.com>
2024-04-15 20:00:09 +0530

    Update 0852-peak-index-in-a-mountain-array-Binary Search.py

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array-Binary Search.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array-Binary Search.py
@@ -1,3 +1,10 @@
 class Solution:
     def peakIndexInMountainArray(self, A):
-        return A.index(max(A))
+        l, r = 0, len(A) - 1
+        while l < r:
+            m = (l + r) / 2
+            if A[m] < A[m + 1]:
+                l = m + 1
+            else:
+                r = m
+        return l
