bb772e27bf52daf0e1b786a4ac238f5509b7d1bb
Uday Kiran <kiranu941@gmail.com>
2024-04-25 00:17:52 +0530

    Time: 69 ms (11.66%), Space: 16.7 MB (6.84%) - LeetHub

---
+++ 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.py
@@ -0,0 +1,36 @@
+class Solution:
+    def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+        mappings = defaultdict(list)
+        N= len(words)
+        for idx, letter in enumerate(words):
+            mappings[groups[idx]].append(idx)
+        print(mappings)
+        idx = 0
+        path = []
+        current = 0
+        while idx < N:
+            find = bisect_left(mappings[current], idx)
+            if find >= len(mappings[current]):
+                break
+            else:
+                idx = mappings[current][find]
+                path.append(words[idx])
+                if current == 0:
+                    current = 1
+                else:
+                    current = 0
+        path2 = []
+        current = 1
+        idx = 0
+        while idx < N:
+            find = bisect_left(mappings[current], idx)
+            if find >= len(mappings[current]):
+                break
+            else:
+                idx = mappings[current][find]
+                path2.append(words[idx])
+                if current == 0:
+                    current = 1
+                else:
+                    current = 0
+        return path if len(path)  > len(path2) else path2
