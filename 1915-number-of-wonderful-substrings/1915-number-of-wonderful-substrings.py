7b014e13f877c50b875556b3008249febde34617
Uday Kiran <kiranu941@gmail.com>
2024-04-30 19:06:34 +0530

    Time: 1625 ms (25.96%), Space: 17.6 MB (50.96%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.py
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.py
@@ -1,10 +1,17 @@
 class Solution:
-    def wonderfulSubstrings(self, word):
-        count = [1] + [0] * 1024
-        res = cur = 0
-        for c in word:
-            cur ^= 1 << (ord(c) - ord('a'))
-            res += count[cur]
-            res += sum(count[cur ^ (1 << i)] for i in range(10))
-            count[cur] += 1
-        return res
+    def wonderfulSubstrings(self, word: str) -> int:
+        seen_sum = {0: 1}
+        prefix = 0
+        result = 0
+        for char in word:
+            prefix = prefix ^ (1 << (ord(char) - ord('a')))
+            if prefix in seen_sum:
+                result += seen_sum[prefix]
+            if prefix in seen_sum:
+                seen_sum[prefix] += 1
+            else:
+                seen_sum[prefix] = 1
+            for j in range(10):
+                if (prefix ^ (1 << j)) in seen_sum:
+                    result += seen_sum[prefix ^ (1 << j)]
+        return result
