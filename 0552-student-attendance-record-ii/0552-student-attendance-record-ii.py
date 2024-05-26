86a71efc78969a11fa1fc087d4c02835af25f946
Uday Kiran <kiranu941@gmail.com>
2024-05-26 12:09:57 +0530

    Time: 645 ms (75.83%), Space: 115.8 MB (11.38%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.py
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.py
@@ -1,12 +1,13 @@
+cache = {}
 class Solution:
-    @cache
     def checkRecord(self, n: int) -> int:
         MOD = 10**9 + 7
-        @cache
         def go(n,A,L):
             if n == 0:
                 return 1
             total = 0
+            if (n,A,L) in cache:
+                return cache[(n,A,L)]
             for char in ('A','L','P'):
                 if A == 1 and char == 'A':
                     continue
@@ -18,5 +19,7 @@ class Solution:
                     total += go(n-1,1,0)
                 else:
                     total += go(n-1,A,L + 1)
-            return total % MOD
+            cache[(n,A,L)] = total % MOD
+            return cache[(n,A,L)]
+
         return go(n,0,0)
