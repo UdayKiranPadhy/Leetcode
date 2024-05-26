ad3490acbd653d3fb64f286ef7cc6e90240763c9
Uday Kiran <kiranu941@gmail.com>
2024-05-26 15:40:46 +0530

    Time: 265 ms (100%), Space: 19.4 MB (100%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.py
+++ 3163-string-compression-iii/3163-string-compression-iii.py
@@ -3,22 +3,22 @@ class Solution:
         if not word:
             return ""
         
-        comp = []
+        res = ""
         count = 1
 
         for i in range(1, len(word)):
             if word[i] == word[i - 1]:
                 count += 1
                 if count == 9:
-                    comp.append(f"{count}{word[i - 1]}")
+                    res += f"{count}{word[i-1]}"
                     count = 0
             else:
                 if count > 0:
-                    comp.append(f"{count}{word[i - 1]}")
+                    res += f"{count}{word[i-1]}"
                 count = 1
         
         # Handle the last sequence
         if count > 0:
-            comp.append(f"{count}{word[-1]}")
+            res += f"{count}{word[-1]}"
 
-        return ''.join(comp)
+        return res
