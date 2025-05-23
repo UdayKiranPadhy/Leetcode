aa41974dcdfe06bbf6233cc873a660d6634b915e
Uday <58071209+UdayKiranPadhy@users.noreply.github.com>
2025-05-23 21:25:32 +0530

    Time: 218 ms (12.07%), Space: 31.3 MB (13.79%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
@@ -1,24 +1,22 @@
 class Solution:
+    def maxSumOfNodes(self, index, isEven, nums, k, memo):
+        if index == len(nums):
+            # If the operation is performed on an odd number of elements return INT_MIN
+            return 0 if isEven == 1 else -float("inf")
+        if memo[index][isEven] != -1:
+            return memo[index][isEven]
+
+        # No operation performed on the element
+        noXorDone = nums[index] + self.maxSumOfNodes(index + 1, isEven, nums, k, memo)
+        # XOR operation is performed on the element
+        xorDone = (nums[index] ^ k) + self.maxSumOfNodes(
+            index + 1, isEven ^ 1, nums, k, memo
+        )
+
+        # Memoize and return the result
+        memo[index][isEven] = max(xorDone, noXorDone)
+        return memo[index][isEven]
+
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        sumVal = 0
-        count = 0
-        positiveMinimum = 1 << 30
-        negativeMaximum = -1 * (1 << 30)
-
-        for nodeValue in nums:
-            operatedNodeValue = nodeValue ^ k
-            sumVal += nodeValue
-            netChange = operatedNodeValue - nodeValue
-            if netChange > 0:
-                positiveMinimum = min(positiveMinimum, netChange)
-                sumVal += netChange
-                count += 1
-            else:
-                negativeMaximum = max(negativeMaximum, netChange)
-
-        # If the number of positive netChange values is even, return the sum.
-        if count % 2 == 0:
-            return sumVal
-
-        # Otherwise return the maximum of both discussed cases.
-        return max(sumVal - positiveMinimum, sumVal + negativeMaximum)
+        memo = [[-1] * 2 for _ in range(len(nums))]
+        return self.maxSumOfNodes(0, 1, nums, k, memo)
