e04480c7b16f666a171ef56955a08cb141b8352c
Uday <58071209+UdayKiranPadhy@users.noreply.github.com>
2026-01-10 06:04:03 +0530

    Time: 239 ms (92.49%), Space: 19.3 MB (74.62%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -1,14 +1,23 @@
 class Solution:
-    def minimumDeleteSum(self, s1, s2):
-        s1, s2 = [0] + [ord(i) for i in s1], [0] + [ord(i) for i in s2]
-        n1, n2 = len(s1), len(s2)
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        m, n = len(s1), len(s2)
+        dp = [0] * (n + 1)
+
+        for j in range(1, n + 1):
+            dp[j] = dp[j - 1] + ord(s2[j - 1])
         
-        @lru_cache(None)
-        def dp(i1, i2):
-            if i1 == 0 and i2 == 0: return 0
-            if i1 == 0: return dp(i1, i2-1) + s2[i2]
-            if i2 == 0: return dp(i1-1, i2) + s1[i1]
-            if s1[i1] == s2[i2]: return dp(i1-1, i2-1)
-            return min(dp(i1-1, i2) + s1[i1], dp(i1, i2-1) + s2[i2])
+        for i in range(1, m + 1):
+            prev = dp[0]
+            dp[0] += ord(s1[i - 1])
+
+            for j in range(1, n + 1):
+                temp = dp[j]
+
+                if s1[i - 1] == s2[j - 1]:
+                    dp[j] = prev
+                else:
+                    dp[j] = min(dp[j] + ord(s1[i - 1]), dp[j - 1] + ord(s2[j - 1]))
+                
+                prev = temp
         
-        return dp(n1 - 1, n2 - 1)
+        return dp[n]
