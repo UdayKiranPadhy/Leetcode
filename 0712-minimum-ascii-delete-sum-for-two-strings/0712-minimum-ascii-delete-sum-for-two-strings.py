c4fd312dc2ad1a2e5fb94aab2fc827328a2e46b1
Uday <58071209+UdayKiranPadhy@users.noreply.github.com>
2026-01-11 06:14:45 +0530

    Time: 639 ms (25.56%), Space: 225.3 MB (5.14%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -1,23 +1,18 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-        m, n = len(s1), len(s2)
-        dp = [0] * (n + 1)
-
-        for j in range(1, n + 1):
-            dp[j] = dp[j - 1] + ord(s2[j - 1])
+        N , M = len(s1), len(s2)
         
-        for i in range(1, m + 1):
-            prev = dp[0]
-            dp[0] += ord(s1[i - 1])
-
-            for j in range(1, n + 1):
-                temp = dp[j]
-
-                if s1[i - 1] == s2[j - 1]:
-                    dp[j] = prev
-                else:
-                    dp[j] = min(dp[j] + ord(s1[i - 1]), dp[j - 1] + ord(s2[j - 1]))
-                
-                prev = temp
+        @cache
+        def go(i,j):
+            if i == N and j == M:
+                return 0
+            if i == N:
+                return ord(s2[j]) + go(i,j+1)
+            if j == M:
+                return ord(s1[i]) + go(i+1,j)
+            if s1[i] == s2[j]:
+                return go(i+1,j+1)
+            else:
+                return min(ord(s1[i]) + go(i+1,j), ord(s2[j]) + go(i, j + 1) )
         
-        return dp[n]
+        return go(0,0)
