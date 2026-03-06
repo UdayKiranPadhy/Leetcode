76655f3daf4653fdd39ee61a6a081bdf9daa8d7b
Uday <58071209+UdayKiranPadhy@users.noreply.github.com>
2026-03-06 09:39:38 +0530

    Time: 1123 ms (33.33%), Space: 162.7 MB (10.3%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -0,0 +1,11 @@
+class Solution:
+    def winnerSquareGame(self, n):
+        
+        @lru_cache(None)
+        def dfs(state):
+            if state == 0: return False
+            for i in range(1, int(math.sqrt(state))+1):
+                if not dfs(state - i*i): return True
+            return False
+        
+        return dfs(n)
