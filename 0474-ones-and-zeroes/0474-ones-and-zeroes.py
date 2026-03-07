a921cd27f13c5d67906d3bcce46caadbcc5bedb1
Uday <58071209+UdayKiranPadhy@users.noreply.github.com>
2026-03-07 22:56:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        N = len(strs)
+
+        ones = {}
+        zeros = {}
+
+        for i in range(N):
+            ones[i] = strs[i].count('1')
+            zeros[i] = strs[i].count('0')
+
+        @cache
+        def go(index, m, n):
+            if index >= N:
+                return 0
+            if m < 0 or n < 0:
+                return 0
+            op1 = -float('inf')
+            if zeros[index] <= m and ones[index] <= n:
+                op1 = 1 + go(index + 1, m - zeros[index], n - ones[index])
+            op2 = go(index + 1, m, n)
+            return max(op1, op2)
+
+        return go(0, m, n)
