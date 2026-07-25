125471c2b8ea48e516e6b0a8c776993c11c5d722
Uday <58071209+UdayKiranPadhy@users.noreply.github.com>
2026-07-25 22:31:59 +0530

    Time: 0 ms (100%), Space: 19.5 MB (20.52%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        
+        def go(index):
+            if index < 2:
+                return 0
+            
+            if nums[index] - nums[index-1] == nums[index-1] - nums[index-2]:
+                return 1 + go(index-1)
+            
+            return 0
+        
+        total = 0
+        for i in range(len(nums)):
+            total += go(i)
+        
+        return total
