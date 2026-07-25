8ddd2c8a2d45f5839c0a256c17419617a2ed3a49
Uday <58071209+UdayKiranPadhy@users.noreply.github.com>
2026-07-25 23:00:23 +0530

    Time: 339 ms (29.02%), Space: 137.9 MB (13.98%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,3 +1,13 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        
+        @cache
+        def dp(l, r):
+            if l >= r:
+                return piles[l]
+            
+            take_left = piles[l] - dp(l+1, r)
+            take_right = piles[r] - dp(l, r-1)
+            return max(take_left , take_right)
+        
+        return dp(0, len(piles) - 1) >= 0
