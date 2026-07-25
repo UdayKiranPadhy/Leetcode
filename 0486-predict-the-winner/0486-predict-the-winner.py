50b685854062163bfb8ee9ae9d34d84a55d46b47
Uday <58071209+UdayKiranPadhy@users.noreply.github.com>
2026-07-25 22:48:06 +0530

    Time: 1673 ms (12.91%), Space: 19.2 MB (78.52%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,12 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        
+        def go(l, r):
+            if l == r:
+                return nums[l]
+            
+            take_left = nums[l] - go(l+1, r)
+            take_right = nums[r] - go(l, r-1)
+            return max(take_left, take_right)
+        
+        return go(0,len(nums)-1) >= 0
